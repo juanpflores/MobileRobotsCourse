c1f507836d3af5595a53f31ccf50f49506651076
Oscar Sandoval Penilla <oscarsan.penilla@gmail.com>
2018-08-25 13:58:23 -0500

    Se modifica el metodo __str__

--- catkin_ws/src/students/sandoval_penilla/scripts/practica_01.py
+++ catkin_ws/src/students/sandoval_penilla/scripts/practica_01.py
@@ -5,11 +5,11 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
 class Practica01(object):
-    mov = False;
-    obs = False;
-    
-    
-    
+
+    mov = False
+    obs = False
+    front_laser = 0
+
     def __init__(self):
         #creamos un nodo
         print("Creando Nodo")
@@ -26,29 +26,22 @@ class Practica01(object):
         rospy.Subscriber('/hardware/scan',
                          LaserScan,
                          self.callback_scan)
-                         
     def callback_move(self,msg):
         self.mov = msg.data
         print("Recibed: {}".format(self.mov))
-        
+
     def callback_scan(self,msg):
-        front_laser = msg.ranges[len(msg.ranges)/2]
-        self.obs = front_laser < 0.5        
-        if self.obs:
-            #self.mov = False
-            print("--------")
-            print("Robot Blocked?: {}".format(self.mov))
-            print("Obstacle?: {}".format(self.obs))
-            print("Alert!!! Distance: {:9.2f}".format(front_laser))
-        else:
-            #self.mov = True
-            print("--------")
-            print("Robot Blocked?: {}".format(self.mov))
-            print("Obstacle?: {}".format(self.obs))
-            print("Distance: {:9.2f}".format(front_laser))
-            
-    
-        
+        self.front_laser = msg.ranges[len(msg.ranges)/2]
+        self.obs = self.front_laser < 0.5
+        print(self)
+
+    def __str__(self):
+        return  """---------------
+Robot Blocked?: {}
+Obstacle?:      {}
+Distance:       {:.2f}""".format(self.mov,self.obs,self.front_laser)
+
+
     def main(self):
         rate = rospy.Rate(15)
         while not rospy.is_shutdown():
@@ -58,12 +51,10 @@ class Practica01(object):
                 msg.linear.x = 0
             self.pub.publish(msg)
             rate.sleep()
-            
+
 if __name__ == "__main__":
     try:
         prac = Practica01()
         prac.main()
-        
-        
     except rospy.ROSInterruptException:
         pass
