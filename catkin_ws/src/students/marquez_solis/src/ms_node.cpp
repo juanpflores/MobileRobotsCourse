7b40ec7947b0c1b944ff4be54ac56f94a9826c28
cell <gerar1995@gmail.com>
2018-08-30 23:58:09 -0500

    Se añadieron comentarios

--- catkin_ws/src/students/marquez_solis/src/ms_node.cpp
+++ catkin_ws/src/students/marquez_solis/src/ms_node.cpp
@@ -3,33 +3,40 @@
 #include "std_msgs/Bool.h"
 #include "geometry_msgs/Twist.h"
 #include "sensor_msgs/LaserScan.h"
+//Estado del robot
 class Context{
+	//Subscriptores
 	ros::Subscriber subs_forward;
 	ros::Subscriber subs_scan;
 public:
 	bool fwd;
 	bool obstacle;
+	//Callback de accion hacia adelante
 	void forward_callback(const std_msgs::Bool::ConstPtr& bit){
-		std::cout << (int)bit->data<<std::endl;
+		//std::cout << (int)bit->data<<std::endl;
 		fwd = bit->data;
 	}
+	//Callback de scan
 	void laser_callback(const sensor_msgs::LaserScan::ConstPtr& scan){
 		auto middle = scan->ranges[scan->ranges.size()/2];
 		obstacle = middle < 0.5;
 	}
+	//Registrar los subscriptores
 	void subscribe_to(ros::NodeHandle &n){
 		subs_forward = n.subscribe<std_msgs::Bool>("/forward",2,&Context::forward_callback,this);
 		subs_scan = n.subscribe<sensor_msgs::LaserScan>("/hardware/scan",2,&Context::laser_callback,this);
 	}
 };
 int main(int argc,char** argv){
-	Context q;
 	ros::init(argc,argv,"ms_node");
 	ros::NodeHandle n;
-	q.subscribe_to(n);
 	ros::Rate rate(10);
+	Context q;
+	q.subscribe_to(n);
 	ros::Publisher pub_cmd_vel = n.advertise<geometry_msgs::Twist>("/hardware/mobile_base/cmd_vel",1);
+	//Velocidad de detener
 	geometry_msgs::Twist twist_stop;
+	//Velociad de adelante
 	geometry_msgs::Twist twist_forward;
 	twist_forward.linear.x = 0.2;
 	while (ros::ok()){
