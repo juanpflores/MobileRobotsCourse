72d204324d293c42fce4b7ea818cb0fe325e2442
Gerardo <gerar1995@gmail.com>
2018-09-18 09:44:28 -0500

    trabajo en angular

--- catkin_ws/src/students/marquez_solis/src/practica_02.cpp
+++ catkin_ws/src/students/marquez_solis/src/practica_02.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
-
+#include <cmath>
 #include "ros/ros.h"
 #include "std_msgs/Bool.h"
 #include "geometry_msgs/Twist.h"
 //Bibliotecas para manejar las transformaciones
 #include "tf2/LinearMath/Transform.h"
+#include "tf2/utils.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 #include "tf2_ros/transform_listener.h"
 
 using tf2::Vector3;
 using tf2::Transform;
 using geometry_msgs::Twist;
+constexpr double PI = 3.1415926535897932384626433832795028841971;
 
-Twist omni_control(const Transform &pose, const Twist& target, double k = 0.25,double tolerance = 0.15, double turn_ratio = 1);
+Twist omni_control(const Transform &pose, const Twist& target, double k = 0.25,double tolerance = 0.05, double turn_ratio = 0.05);
 
 class Practica2{
     ros::Publisher pub_cmd_vel;
@@ -37,6 +39,7 @@ public:
            omni = tipo_arg == "omni";
            n_private.getParam("x",target.linear.x);
            n_private.getParam("y",target.linear.y);
+           n_private.getParam("w",target.angular.z);
         }
     void loop(){
         unsigned long k;
@@ -79,20 +82,29 @@ int main(int argc,char** argv){
         app.loop();
 	return 0;
 }
-
+double norm_angle (double angle){
+    double trunc = fmod(angle,2*PI);
+    if (trunc > PI) return -2*PI + trunc;
+    else if (trunc < -PI) return 2*PI + trunc;
+    else return trunc;
+}
 //Funcion de control omnidireccional
 Twist omni_control(const Transform &pose, const Twist& target, double k,double tolerance, double turn_ratio){
     Twist out;
     //Obtener direccion absoluta deseada
     auto delta = Vector3(target.linear.x,target.linear.y,0) - pose.getOrigin();
-    auto dist_s = tf2::tf2Dot(delta,delta);
+    double dist_s = tf2::tf2Dot(delta,delta);
     delta.normalize();
     auto rot = pose.getRotation();
+    double angle = tf2::getYaw(rot);
     //Invertir rotación
     rot[3] = -rot[3];
     //Aplicar rotacción y escalar
     auto vel_vector = tf2::quatRotate (rot,delta)*k*(1-1/(dist_s/(tolerance*tolerance)+1));
+    double angle_error = norm_angle(angle - target.angular.z);
+    
     out.linear = tf2::toMsg(vel_vector);
     out.linear.z = 0;
+    out.angular.z = angle_error*turn_ratio*(1-1/(angle_error*angle_error + 1));
     return out;
 }
