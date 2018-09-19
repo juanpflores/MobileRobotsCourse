fdb67f453fc6084dc38d4bdb61a81f7cbdc01b0f
Gerardo <gerar1995@gmail.com>
2018-09-19 01:14:47 -0500

    practica 02 funcionando enteramente

--- catkin_ws/src/students/marquez_solis/src/practica_02.cpp
+++ catkin_ws/src/students/marquez_solis/src/practica_02.cpp
@@ -14,7 +14,18 @@ using tf2::Transform;
 using geometry_msgs::Twist;
 constexpr double PI = 3.1415926535897932384626433832795028841971;
 
-Twist omni_control(const Transform &pose, const Twist& target, double k = 0.25,double tolerance = 0.05, double turn_ratio = 0.1);
+Twist omni_control(const Transform &pose, const Twist& target, double k = 0.25,double tolerance = 0.05, double turn_ratio = 0.5, double turn_tol = 0.1);
+Twist diff_control(const Transform &pose, const Twist& target, double k = 0.25,double tolerance = 0.05, double turn_ratio = 0.5, double turn_tol = 0.1);
+//Distribucion coseno para intervalos de tolerancia, principal ventaja: es una funcion compacta
+inline double raised_cosine_norm(double arg){
+    return abs(arg) < 1 ? 0.5 + 0.5*cos(arg*PI) : 0; 
+}
+inline double single_sine_norm(double arg){
+    if (arg < -1) return -1;
+    if (arg > 1) return 1;
+    auto a = sin(arg*PI/2);
+    return a*a*a;
+}
 
 class Practica2{
     ros::Publisher pub_cmd_vel;
@@ -59,14 +70,16 @@ public:
                     
                     //Lo que se busca
                     tf2::fromMsg(geom_tf,tf_main);
-                    
-                    
-                    Twist vel_target = omni_control(tf_main,target);
-                    pub_cmd_vel.publish(vel_target );
+                    Twist vel_out;
+                    if (omni)
+                        vel_out = omni_control(tf_main,target);
+                    else
+                        vel_out = diff_control(tf_main,target);
+                    pub_cmd_vel.publish(vel_out );
                     
                     //Sólo mostrar el estado cada 8 tiempos
                     if(k % 8 == 0){
-                        //std::cout << geom_tf << std::endl;
+                        std::cout << geom_tf << std::endl;
                     }
                 //Ignorar excepciones (mala idea en general)
                 }catch(...){
@@ -89,7 +102,7 @@ double norm_angle (double angle){
     else return trunc;
 }
 //Funcion de control omnidireccional
-Twist omni_control(const Transform &pose, const Twist& target, double k,double tolerance, double turn_ratio){
+Twist omni_control(const Transform &pose, const Twist& target, double k,double tolerance, double turn_ratio, double turn_tol){
     Twist out;
     //Obtener direccion absoluta deseada
     auto delta = Vector3(target.linear.x,target.linear.y,0) - pose.getOrigin();
@@ -99,13 +112,29 @@ Twist omni_control(const Transform &pose, const Twist& target, double k,double t
     double angle = tf2::getYaw(rot);
     //Invertir rotación
     rot[3] = -rot[3];
-    //Aplicar rotacción y escalar
+    //Aplicar rotacción y escalar por velocidad e intervalo de tolerancia
     auto vel_vector = tf2::quatRotate (rot,delta)*k*(1-1/(dist_s/(tolerance*tolerance)+1));
     double angle_error = norm_angle( target.angular.z - angle);
     
-    std::cout << "angle " << angle << " target " << target.angular.z << "error" << angle_error << std::endl;
+//    std::cout << "angle " << angle << " target " << target.angular.z << "error" << angle_error << std::endl;
     out.linear = tf2::toMsg(vel_vector);
     out.linear.z = 0;
-    out.angular.z = angle_error*turn_ratio*(1-1/(angle_error*angle_error*PI*PI*4 + 1));
+    //Multiplicar dirección de giro por tasa de giro e intervalo de tolerancia
+    out.angular.z = (std::signbit(angle_error)?-1:1)*turn_ratio*(1-raised_cosine_norm(angle_error/turn_tol));
+    return out;
+}
+Twist diff_control(const Transform &pose, const Twist& target, double k,double tolerance, double turn_ratio, double turn_tol){
+    Twist out;
+    auto delta = Vector3(target.linear.x,target.linear.y,0) - pose.getOrigin();
+    double dist_s = delta[0] + delta[1];
+    auto rot = pose.getRotation();
+    double angle = tf2::getYaw(rot);
+    double angle_error = norm_angle( atan2(delta[1],delta[0]) - angle);
+    auto vel_vector = Vector3(k,0,0)*raised_cosine_norm(angle_error/turn_tol)*(1-raised_cosine_norm(dist_s/tolerance));
+    //  std::cout << "angle " << angle << " target " <<  atan2(delta[1],delta[0]) << "error" << angle_error << std::endl;
+    out.linear = tf2::toMsg(vel_vector);
+    out.linear.z = 0;
+    //Multiplicar dirección de giro por tasa de giro e intervalo de tolerancia
+    out.angular.z = turn_ratio*single_sine_norm(angle_error/turn_tol);
     return out;
 }
