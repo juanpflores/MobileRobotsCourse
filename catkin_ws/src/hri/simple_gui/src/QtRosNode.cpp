0ccb713dc4c3e25527ed6abe63e2be9957198605
Mark The Magnificent <mnegretev@gmail.com>
2018-09-24 21:14:38 -0500

    Modify GUI to add controls for path planning

--- catkin_ws/src/hri/simple_gui/src/QtRosNode.cpp
+++ catkin_ws/src/hri/simple_gui/src/QtRosNode.cpp
@@ -19,7 +19,11 @@ QtRosNode::~QtRosNode()
 void QtRosNode::run()
 {    
     ros::Rate loop(30);
-    pubCmdVel  = n->advertise<geometry_msgs::Twist>("/hardware/mobile_base/cmd_vel", 10);
+    pubCmdVel   = n->advertise<geometry_msgs::Twist>("/hardware/mobile_base/cmd_vel", 10);
+    cltBFS      = n->serviceClient<nav_msgs::GetPlan>("/navigation/path_planning/breadth_first_search");
+    cltDFS      = n->serviceClient<nav_msgs::GetPlan>("/navigation/path_planning/depth_first_search");
+    cltDijkstra = n->serviceClient<nav_msgs::GetPlan>("/navigation/path_planning/dijkstra_search");
+    cltAStar    = n->serviceClient<nav_msgs::GetPlan>("/navigation/path_planning/a_star_search");  
 
     int pub_zero_counter = 5;
     while(ros::ok() && !this->gui_closed)
@@ -69,4 +73,56 @@ void QtRosNode::stop_publishing_cmd_vel()
     cmd_vel.linear.y = 0;
     cmd_vel.angular.z = 0;
     publishing_cmd_vel = false;
+}
+
+bool QtRosNode::call_breadth_first_search(float start_x, float start_y, float goal_x, float goal_y)
+{
+    nav_msgs::GetPlan srv;
+    srv.request.start.pose.position.x = start_x;
+    srv.request.start.pose.position.y = start_y;
+    srv.request.goal.pose.position.x = goal_x;
+    srv.request.goal.pose.position.y = goal_y;
+    return cltBFS.call(srv);
+}
+
+bool QtRosNode::call_depth_first_search(float start_x, float start_y, float goal_x, float goal_y)
+{
+    nav_msgs::GetPlan srv;
+    srv.request.start.pose.position.x = start_x;
+    srv.request.start.pose.position.y = start_y;
+    srv.request.goal.pose.position.x = goal_x;
+    srv.request.goal.pose.position.y = goal_y;
+    return cltDFS.call(srv);
+}
+
+bool QtRosNode::call_dijkstra_search(float start_x, float start_y, float goal_x, float goal_y)
+{
+    nav_msgs::GetPlan srv;
+    srv.request.start.pose.position.x = start_x;
+    srv.request.start.pose.position.y = start_y;
+    srv.request.goal.pose.position.x = goal_x;
+    srv.request.goal.pose.position.y = goal_y;
+    return cltDijkstra.call(srv);
+}
+
+bool QtRosNode::call_a_star_search(float start_x, float start_y, float goal_x, float goal_y)
+{
+    nav_msgs::GetPlan srv;
+    srv.request.start.pose.position.x = start_x;
+    srv.request.start.pose.position.y = start_y;
+    srv.request.goal.pose.position.x = goal_x;
+    srv.request.goal.pose.position.y = goal_y;
+    return cltAStar.call(srv);
+}
+
+void QtRosNode::get_robot_pose(float& robot_x, float& robot_y, float& robot_a)
+{
+    tf::StampedTransform t;
+    tf::Quaternion q;
+    tf_listener.waitForTransform("map", "base_link", ros::Time::now(), ros::Duration(0.5));
+    tf_listener.lookupTransform("map", "base_link", ros::Time(0), t);
+    robot_x = t.getOrigin().x();
+    robot_y = t.getOrigin().y();
+    q = t.getRotation();
+    robot_a = atan2(q.z(), q.w())*2;
 }
