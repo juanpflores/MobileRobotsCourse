5a44fecd99d7e86986ea9c3863f0c61fb631e47e
Mark The Magnificent <mnegretev@gmail.com>
2018-09-24 21:09:46 -0500

    Modify GUI to add controls for path planning

--- catkin_ws/src/hri/simple_gui/src/QtRosNode.h
+++ catkin_ws/src/hri/simple_gui/src/QtRosNode.h
@@ -4,8 +4,9 @@
 #include <QThread>
 #include "ros/ros.h"
 #include "std_msgs/Float32MultiArray.h"
-#include "std_msgs/UInt8MultiArray.h"
 #include "geometry_msgs/Twist.h"
+#include "nav_msgs/GetPlan.h"
+#include "tf/transform_listener.h"
 
 class QtRosNode : public QThread
 {
@@ -15,7 +16,13 @@ public:
     ~QtRosNode();
 
     ros::NodeHandle* n;
-    ros::Publisher  pubCmdVel;
+    ros::Publisher        pubCmdVel;
+    ros::ServiceClient    cltBFS;
+    ros::ServiceClient    cltDFS;
+    ros::ServiceClient    cltDijkstra;
+    ros::ServiceClient    cltAStar;
+    tf::TransformListener tf_listener;
+    
     geometry_msgs::Twist cmd_vel;
     bool publishing_cmd_vel;
     bool gui_closed;
@@ -26,6 +33,12 @@ public:
     void publish_cmd_vel(float linear_frontal, float linear_lateral, float angular);
     void start_publishing_cmd_vel(float linear_frontal, float linear_lateral, float angular);
     void stop_publishing_cmd_vel();
+    bool call_breadth_first_search(float start_x, float start_y, float goal_x, float goal_y);
+    bool call_depth_first_search(float start_x, float start_y, float goal_x, float goal_y);
+    bool call_dijkstra_search(float start_x, float start_y, float goal_x, float goal_y);
+    bool call_a_star_search(float start_x, float start_y, float goal_x, float goal_y);
+
+    void get_robot_pose(float& robot_x, float& robot_y, float& robot_a);
     
 signals:
     void updateGraphics();
