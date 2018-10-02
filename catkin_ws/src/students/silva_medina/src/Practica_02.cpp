e77fc3fa1917d4f63b561fafd0df1337f703e105
Panchuy <you@example.com>
2018-10-01 19:47:46 -0500

    Se suben archivos Practica03 y correccion Practica02)

--- catkin_ws/src/students/silva_medina/src/Practica_02.cpp
+++ catkin_ws/src/students/silva_medina/src/Practica_02.cpp
@@ -8,10 +8,7 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
 #include "tf/transform_listener.h"
-#include <math.h>
-#define NOMBRE "silva_medina"
-#define PI 3.1415926535897
-#define vmax 0.5
+#define NOMBRE "Silva Medina"
 
 int main(int argc, char** argv)
 {
@@ -32,17 +29,7 @@ int main(int argc, char** argv)
     float goal_y;
     float goal_a;
     std::string control_type = "";
-	
-    //Variables utilizadas para el cálculo de errores
 
-    float error_x,error_y,error_a;
-
-    //Declaracion de constantes en la formula
-
-    float k = 0.5, alpha = 0.5, betha = 1.0;
-
-    //
-	
     //Getting parameteres to set goal position and control type.
     n.param<std::string>("type", control_type, "diff"); //The default control type is for a differential base
     n.param<float>("a", goal_a, 0.0);                   //The default goal angle is 0.0 rad
@@ -86,84 +73,53 @@ int main(int argc, char** argv)
 	q = t.getRotation();
 	robot_a = atan2(q.z(), q.w())*2;
 
+	//
+	//TODO:
+	//Calculate the values of msg_cmd_vel according to the control type.
+	//Declare more variables if necessary.
+	//The maximum linear speed must be 0.5 m/s
+	//The maximul angular speed must be 0.5 rad/s
+	//The robot must stop if it is at 0.1 m or less from the goal point
+	float alpha = 0.5;
+	float beta  = 0.1;
+	float v_max = 0.5;
+	float w_max = 0.5;
+	float Kpl   = 1.0;
+	float Kpa   = 1.0;
 
-		
-	/*
-	 * TODO:
-	 *
-	 * Calculate the values of msg_cmd_vel according to the control type.
-	 * (for omnidirectional or differential base)
-	 * Declare more variables if necessary.
-	 * The maximum linear speed must be 0.5 m/s
-	 * The maximul angular speed must be 0.5 rad/s
-	 * The robot must stop if it is at 0.1 m or less from the goal point
-	 */
-	
-	//Se calculan los errores en la velocidad en x,y y z	
-		error_x = goal_x - robot_x;
-		error_y = goal_y - robot_y;	
+	float error_x = goal_x - robot_x;
+	float error_y = goal_y - robot_y;
+	float error_a;
+	float error_m = sqrt(error_x*error_x + error_y*error_y);
+	if(control_type == "omni")
+	    error_a = goal_a - robot_a;
+	else
+	    error_a = atan2(error_y, error_x) - robot_a;
+	if(error_a >  M_PI) error_a -= 2*M_PI;
+	if(error_a < -M_PI) error_a += 2*M_PI;
 
-	//Se calcula el error de ángulo para dejarlo en el rango de PI a -PI
-		if (control_type=="omni")
-	   		error_a = goal_a - robot_a;
-		else
-			error_a = atan2(error_y,error_x) - robot_a;
-	
-		if(error_a < -PI)
-        		error_a += 2*PI;
-    		if (error_a > PI)
-        		error_a -= 2*PI;
-
-
-	if(fabs(error_x>=0.1) || fabs(error_y>=0.1)){//Mientras que el error en x ó en y sea mayor a 0.1 m
-
-	
-	
-		if(control_type == "omni"){//Si el control es de tipo omnidireccional
- 			
-			//Formulas para calcular la velocidad lineal
-        		msg_cmd_vel.linear.x = k * error_x * cos(robot_a) + k * error_y * sin(robot_a);
-        		msg_cmd_vel.linear.y = -k * error_x * sin(robot_a) + k * error_y * cos(robot_a);
-
-			//Si las velocidades lineal es mayor a 0.5, se modifican
-			
-			if (msg_cmd_vel.linear.x > 0.5)
-		        	msg_cmd_vel.linear.x = vmax;
-		        if (msg_cmd_vel.linear.y > 0.5)
-            			msg_cmd_vel.linear.y = vmax;
-        		if (msg_cmd_vel.linear.x < 0.5)
-            		 	msg_cmd_vel.linear.x = -vmax;
-        		if (msg_cmd_vel.linear.y < 0.5)
-            			msg_cmd_vel.linear.y = -vmax;
-
-			
-		}        
-		else{//Si el control es de tipo diferencial
-
-		
-			//Formulas para calcular la velocidad lineal en x y el angulo necesario para moverse
-		
-			msg_cmd_vel.linear.x = vmax * exp(-pow(error_a, 2) / alpha);
-			msg_cmd_vel.angular.z = vmax * (2 / (1 + exp(-(error_a / betha))) - 1);	
-			
-		}
-
-
+	if(control_type == "omni")
+	{
+	    msg_cmd_vel.linear.x  = Kpl * ( error_x * cos(robot_a) + error_y * sin(robot_a));
+	    msg_cmd_vel.linear.y  = Kpl * (-error_x * sin(robot_a) + error_y * cos(robot_a));
+	    msg_cmd_vel.angular.z = Kpa * error_a;
+	    if(msg_cmd_vel.linear.x >  v_max) msg_cmd_vel.linear.x =  v_max;
+	    if(msg_cmd_vel.linear.x < -v_max) msg_cmd_vel.linear.x = -v_max;
+	    if(msg_cmd_vel.linear.y >  v_max) msg_cmd_vel.linear.y =  v_max;
+	    if(msg_cmd_vel.linear.y < -v_max) msg_cmd_vel.linear.y = -v_max;
 	}
-	else{
-		
-		msg_cmd_vel.linear.x =0;
-		msg_cmd_vel.linear.y =0;
-		if(error_a!=0){// Se modifica el ángulo para obtener el deseado
-							
-			msg_cmd_vel.angular.z = 0.5*(goal_a - robot_a);			
-
-					
-		}
-		else{
-		msg_cmd_vel.angular.z =0;		
-		}
-	}	
+	else if(error_m > 0.1)
+	{
+	    msg_cmd_vel.linear.x  = v_max * exp(-error_a*error_a/alpha);
+	    msg_cmd_vel.linear.y  = 0;
+	    msg_cmd_vel.angular.z = w_max * (2 / (1 + exp(-error_a/beta)) - 1);
+	}
+	else
+	{
+	    msg_cmd_vel.linear.x  = 0;
+	    msg_cmd_vel.linear.y  = 0;
+	    msg_cmd_vel.angular.z = 0;
+	}
 
 	pub_cmd_vel.publish(msg_cmd_vel);
 	ros::spinOnce();
@@ -173,4 +129,3 @@ int main(int argc, char** argv)
     return 0;
     
 }
-
