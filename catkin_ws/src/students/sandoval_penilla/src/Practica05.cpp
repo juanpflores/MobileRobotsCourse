80426b8097c74e66f72551eeb059615d67b09fd1
Oscar Sandoval <oscarsan.penilla@gmail.com>
2018-10-17 18:22:36 -0700

    practica 5 resuelta

--- catkin_ws/src/students/sandoval_penilla/src/Practica05.cpp
+++ catkin_ws/src/students/sandoval_penilla/src/Practica05.cpp
@@ -20,14 +20,48 @@ float global_goal_y;
 float robot_x;
 float robot_y;
 float robot_a;
+bool  new_path;
 /*
  * global_plan: stores the path to be tracked.
  * cltPlanPath: service client used to call the service for calculating a path. 
  */
 nav_msgs::Path global_plan;
 ros::ServiceClient clt_plan_path;
-bool new_path;
 
+geometry_msgs::Twist velocidad_diff(float goal_x,float goal_y){
+    // Empleando el codigo de la practica 02 para el robot diferencial
+    float alpha = 0.5;
+    float beta  = 0.12;
+    float vMax  = 0.5;
+    float wMax  = 0.5;
+    geometry_msgs::Twist msg_cmd_vel;
+    float errX  = goal_x - robot_x;
+    float errY  = goal_y - robot_y;
+    float errA  = atan2(errY,errX) - robot_a;
+    // reducimos el angulo al primero y segundo cuadrante
+    if(errA > M_PI) errA -= 2*M_PI;
+    if(errA < -M_PI) errA += 2*M_PI;
+    // Modificamos la velocidad lineal y angular
+    msg_cmd_vel.linear.x = vMax * exp(-errA*errA/alpha);
+    msg_cmd_vel.linear.y = 0;
+    msg_cmd_vel.angular.z = wMax * (2/(1+exp(-errA/beta))-1);
+    // Devolvemos la velocidad lineal y angualar
+    return msg_cmd_vel;
+}
+
+void siguiente_punto(tf::TransformListener& tfListener,float& goal_x,float& goal_y, int& punto_indice ){
+    float err = 0  ;
+    if(punto_indice >= global_plan.poses.size()) punto_indice = global_plan.poses.size()-1;
+    
+    do{
+        goal_x = global_plan.poses[punto_indice].pose.position.x;
+        goal_y = global_plan.poses[punto_indice].pose.position.y;
+        err = sqrt((goal_x-robot_x)*(goal_x-robot_x)+(goal_y-robot_y)*(goal_y-robot_y));
+
+
+    }while(err < 0.25 && ++punto_indice < global_plan.poses.size());
+
+}
 
 void callback_go_to_xya(const std_msgs::Float32MultiArray::ConstPtr& msg)
 {
@@ -47,47 +81,22 @@ void callback_go_to_xya(const std_msgs::Float32MultiArray::ConstPtr& msg)
     srv.request.goal.pose.position.y  = global_goal_y;
     clt_plan_path.call(srv);
     global_plan = srv.response.plan;
+    new_path = true;
 }
 
-geometry_msgs::Twist calculate_speeds_diff(float goal_x,float goal_y){
-    float alpha = 1;
-    float beta  = 0.1;
-    float vMax  = 0.5;
-    float wMax  = 0.5;
-    float errX  = goal_x - robot_x;
-    float errY  = goal_y - robot_y;
-    float errA  = atan2(errY,errX) - robot_a;
-
-    if(errA > M_PI) errA -= 2*M_PI;
-    if(errA < M_PI) errA += 2*M_PI;
-
-    geometry_msgs::Twist msg_cmd_vel;
-    msg_cmd_vel.linear.x = vMax * exp(-errA*errA/alpha);
-    msg_cmd_vel.linear.y = 0;
-    msg_cmd_vel.linear.z = wMax * (2/(1+exp(-errA/beta))-1);
-
-    return msg_cmd_vel;
-}
-
-void get_next_goal(float& goal_x,float& goal_y, int& next_pose_idx, tf::TransformListener& tfListener){
-    float err;
-    if(next_pose_idx >= global_plan.poses.size()) next_pose_idx = global_plan.poses.size()-1;
-    
-    do{
-        goal_x = global_plan.poses[next_pose_idx].pose.position.x;
-        goal_y = global_plan.poses[next_pose_idx].pose.position.y;
-        err = sqrt((goal_x-robot_x)*(goal_x-robot_x)+(goal_y-robot_y)*(goal_y-robot_y));
 
 
-    }while(err < 0.25 && ++next_pose_idx < global_plan.poses.size());
 
+
+float distancia(){
+    return sqrt((global_goal_x - robot_x)*(global_goal_x-robot_x)+(global_goal_y-robot_y)*(global_goal_y-robot_y));
 }
 
 
 
 int main(int argc, char** argv)
 {
-    std::cout << "PRÁCTICA 05 - SEGUIMIENTO DE RUTAS - " << NOMBRE << std::endl;
+    std::cout << "Main - " << NOMBRE << std::endl;
     ros::init(argc, argv, "practica_05");
     ros::NodeHandle n("~");
     ros::Rate loop(20);
@@ -101,11 +110,13 @@ int main(int argc, char** argv)
 
     geometry_msgs::Twist msg_cmd_vel;
 
-	int next_pose_idx = 0;
-    bool moving = false;				
+	int punto_indice = 0;
+    bool meta = false;
+    	
 
     while(ros::ok())
     {
+    //   std::cout << "while " << std::endl;
 	/*
 	 * Instructions needed to get the current robot position.
 	 */
@@ -123,20 +134,24 @@ int main(int argc, char** argv)
 	 * Store the linear and angular speed in msg_cmd_vel.
 	 */
     
-
+    
 
     if(new_path){
+        std::cout << "new path"<< std::endl;
         new_path = false;
-        next_pose_idx = 0;
-        moving = true;
+        punto_indice = 0;
+        meta = true;
     }
-    if(moving){
-        float local_goal_x;
-        float local_goal_y;
-        get_next_goal(local_goal_x, local_goal_y, next_pose_idx,tl);
+    if(meta){
+        std::cout << "meta"<< std::endl;
+        float coord_x;
+        float coord_y;
+        siguiente_punto(tl,coord_x, coord_y, punto_indice);
         float dist_to_goal = sqrt((global_goal_x - robot_x)*(global_goal_x-robot_x)+(global_goal_y-robot_y)*(global_goal_y-robot_y));
-        pub_cmd_vel.publish(calculate_speeds_diff(local_goal_x,local_goal_y));
-        if(dist_to_goal < 0.2 || new_path) moving = false;
+        msg_cmd_vel = velocidad_diff(coord_x,coord_y);
+        std::cout << "x - " << msg_cmd_vel.linear.x << std::endl;
+        pub_cmd_vel.publish(msg_cmd_vel);
+        if(dist_to_goal <= 0.25 || new_path) meta = false;
     }
 
 	//pub_cmd_vel.publish(msg_cmd_vel);
